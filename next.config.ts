586ee15a174a9c9b77dddeefc6780c21fc4cd0d1
PiaoAPCL <48676085+sparkpzch@users.noreply.github.com>
2025-02-26 16:20:28 +0700

    Prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? "/jirawatdamung.github.io/" : "",
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
