abbbc84b9a06363fde596618aa48484bdf49f10d
PiaoAPCL <48676085+sparkpzch@users.noreply.github.com>
2025-02-26 16:52:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
+// Determine if the current environment is production
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/jirawatdamung.github.io/" : "",
+  basePath: isProd ? "/jirawatdamung.github.io" : "",
+  assetPrefix: isProd ? "/jirawatdamung.github.io/" : "",
   images: {
     unoptimized: true,
   },
